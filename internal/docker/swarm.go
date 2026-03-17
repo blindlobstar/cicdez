13a70d0cad6f4ffbaf078b51bc9d25a26d877a13
blindlobstar <boryapictures@gmail.com>
2026-03-18 00:13:03 +0100

    more todos and fixes

--- internal/docker/swarm.go
+++ internal/docker/swarm.go
@@ -11,26 +11,26 @@ import (
 
 var ErrManagerNotFound = errors.New("manager not found")
 
-func GetManagerClient(ctx context.Context, servers map[string]vault.Server, exclude ...string) (client.APIClient, error) {
+func GetManagerClient(ctx context.Context, servers map[string]vault.Server, exclude ...string) (client.APIClient, string, error) {
 	for host, server := range servers {
 		if slices.Contains(exclude, host) {
 			continue
 		}
 		manager, err := NewClientSSH(host, server.Port, server.User, []byte(server.Key))
 		if err != nil {
-			return nil, err
+			return nil, "", err
 		}
 
 		info, err := manager.Info(ctx, client.InfoOptions{})
 		if err != nil {
 			manager.Close()
-			return nil, err
+			return nil, "", err
 		}
 		if !info.Info.Swarm.ControlAvailable {
 			manager.Close()
 			continue
 		}
-		return manager, nil
+		return manager, host, nil
 	}
-	return nil, ErrManagerNotFound
+	return nil, "", ErrManagerNotFound
 }
