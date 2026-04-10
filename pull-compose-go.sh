58269aecf7008b8dcb8bb5e1773e74cb6afa6c8c
blindlobstar <boryapictures@gmail.com>
2026-04-10 15:32:44 +0200

    update compose-go version alongside with subtree

--- pull-compose-go.sh
+++ pull-compose-go.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
 git subtree pull -P external/compose-go git@github.com:compose-spec/compose-go.git "$1" --squash
+# Update version in go.mod
+sed -i '' "s|github.com/compose-spec/compose-go/v2 v[^ ]*|github.com/compose-spec/compose-go/v2 $1|" go.mod
